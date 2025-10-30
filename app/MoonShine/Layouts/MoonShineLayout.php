de2ea8ee4f2817c3bafaa4a54841d635d6bd8627
Shevelev Anatoliy <tolik.breathless@yandex.ru>
2025-10-30 21:59:21 +0700

    feat: MoonShine 4 support

--- app/MoonShine/Layouts/MoonShineLayout.php
+++ app/MoonShine/Layouts/MoonShineLayout.php
@@ -4,67 +4,14 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Layouts;
 
-use MoonShine\Laravel\Layouts\CompactLayout;
-use MoonShine\ColorManager\ColorManager;
-use MoonShine\Contracts\ColorManager\ColorManagerContract;
-use MoonShine\Laravel\Components\Layout\{Locales, Notifications, Profile, Search};
-use MoonShine\UI\Components\{Breadcrumbs,
-    Components,
-    Layout\Flash,
-    Layout\Div,
-    Layout\Body,
-    Layout\Burger,
-    Layout\Content,
-    Layout\Footer,
-    Layout\Head,
-    Layout\Favicon,
-    Layout\Assets,
-    Layout\Meta,
-    Layout\Header,
-    Layout\Html,
-    Layout\Layout,
-    Layout\Logo,
-    Layout\Menu,
-    Layout\Sidebar,
-    Layout\ThemeSwitcher,
-    Layout\TopBar,
-    Layout\Wrapper,
-    When};
+use MoonShine\Laravel\Layouts\AppLayout;
 
-final class MoonShineLayout extends CompactLayout
+final class MoonShineLayout extends AppLayout
 {
     protected function menu(): array
     {
         return [
             //
         ];
-    }
-
-    public function build(): Layout
-    {
-        return Layout::make([
-            Html::make([
-                $this->getHeadComponent(),
-                Body::make([
-                    Wrapper::make([
-                        Div::make([
-                            Flash::make(),
-                            Content::make([
-                                Components::make(
-                                    $this->getPage()->getComponents()
-                                ),
-                            ]),
-
-                            $this->getFooterComponent(),
-                        ])->class('layout-page'),
-                    ]),
-                ])->class('theme-minimalistic'),
-            ])
-                ->customAttributes([
-                    'lang' => $this->getHeadLang(),
-                ])
-                ->withAlpineJs()
-                ->withThemes(alwaysDark: true),
-        ]);
     }
 }
